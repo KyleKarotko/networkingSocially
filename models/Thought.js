f3b36a68422a39e027ab1862920f59287ba192a6
kyle_karotko <kkrtk7@duck.com>
2023-03-12 16:22:18 -0400

    filled in Thought model with keys/values

---
+++ models/Thought.js
@@ -0,0 +1,23 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        
+        createdAt: {
+            type:Date,
+            default: Date.now,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+    }
+)

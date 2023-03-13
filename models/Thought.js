5121dd9008db0e56ba080443df410535c1d2accb
kyle_karotko <kkrtk7@duck.com>
2023-03-12 20:57:54 -0400

    added reactionSchema key/values

--- models/Thought.js
+++ models/Thought.js
@@ -19,5 +19,46 @@ const thoughtSchema = new Schema (
             required: true,
         },
 
+        reactions: [reactionSchema],
+
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(,)
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+module.exports = Thought;
